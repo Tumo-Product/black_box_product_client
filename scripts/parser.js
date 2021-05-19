085b4d9eeddcf5babcc8f294b693b0bbbf30c55a
DavidMcarati <davidcarati@gmail.com>
2021-05-19 23:02:30 +0400

    removed strange fetch function

--- scripts/parser.js
+++ scripts/parser.js
@@ -2,10 +2,43 @@ axios.defaults.baseURL = "https://blackboxbasic.herokuapp.com/";
 
 const parser = {
     fetchModules: async () => {
-        let response = await fetch(config.query_url);
-        return await response.json();
+        let response = dummyJson;       //TODO: Switch to Axios
+        return response;
     },
     axiosGet: async (request) => {
         return axios.get(request);
     }
 }
+
+
+
+//----------------------vvvvvvv------------DUMMY JSON DATA GOES HERE------------vvvvvvv---------------------------------
+
+let dummyJson = {
+    "modules": {
+        "qz_module" : {
+            "name": "Quiz",
+            "img": "images/quiz.png",
+            "searchbar": true,
+            "addButton": true,
+            "getRequest": "/quiz/list"
+        },
+        "cc_module" : {
+            "name": "Calculator",
+            "img": "images/calculator.png"
+        }
+    },
+    "modulesArr": [
+        {
+            "name": "Quiz",
+            "img": "images/quiz.png",
+            "searchbar": true,
+            "addButton": true,
+            "getRequest": "/quiz/list"
+        },
+        {
+            "name": "Calculator",
+            "img": "images/calculator.png"
+        }
+    ]
+};
