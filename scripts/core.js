fb079eb99e68e0aa352b4e269e1b0b03e64970f3
davidmcarati <67065389+davidmcarati@users.noreply.github.com>
2021-05-21 18:15:12 +0400

    Ready for requests.

--- scripts/core.js
+++ scripts/core.js
@@ -1,6 +1,14 @@
-$(async function () {
+$(function () {
+    OnStartup();
+})
+
+let OnStartup = () => {
+    OpenLoginScreen();
+}
+
+let OpenLoginScreen = async () => {
     await page_manager.activateLoading();
     await page_manager.openView("login");
     await script_loader.loadScriptList("login");
     await page_manager.disableLoading();
-})
+}
--- scripts/login/login_flow.js
+++ scripts/login/login_flow.js
@@ -1,3 +1,34 @@
 $(function () {
-    console.log("ASHOT");
+    login_flow.OnStartup();
 })
+
+const login_flow = {
+    error_element       : undefined,
+    OnStartup           : () => {
+        error_element       = document.getElementById("error_p");
+    },
+
+    OnValueChange       : () => {
+        login_flow.SetErrorTextState(false);
+    },
+
+    OnLoginPress        : () => {
+        let loginData = {
+            email   :   "",
+            pass    :   ""
+        }
+        loginData.email     = document.getElementById("email").value;
+        loginData.pass      = document.getElementById("pass").value;
+        let validation = {
+            filled  : false,
+            long    : false
+        }
+        validation.filled   = loginData.email !== "" && loginData.pass !== "";
+        validation.long     = loginData.pass.length >= 4;
+        login_flow.SetErrorTextState(!validation.filled || !validation.long);
+    },
+
+    SetErrorTextState   :   (state) => {
+        error_element.style.display = state ? "block" : "none";
+    }
+}
