ffad0039adc563e5b07fd33f37bbf37a62f57f03
vbalyan02 <vitaliy.balyan@tumo.org>
2021-06-08 14:54:53 +0400

    Switched Input fields and Texts to Input Areas

--- scripts/AllControllerSystem/ac_details.js
+++ scripts/AllControllerSystem/ac_details.js
@@ -20,29 +20,47 @@ const ac_details = {
 }
 
 const dt_Handlers = {
-    calculator_handler : {
+    calculator_handler  : {
+        temp_obj    : {},
         onSelect    : async (obj) => {
             ac_details.show_container();
             await ac_details.load_details();
             ac_details.start_loading();
-            let html = dt_Handlers.calculator_handler.generate_details(obj);
+            // let html = dt_Handlers.calculator_handler.generate_details(obj);
+            temp_obj = obj;
+
+            let html = await dt_Handlers.calculator_handler.generate_details(temp_obj);
             ac_details.clean_details();
             $("#item_container").html(html);
+            // let add_button = document.getElementById("add_question");
+            // add_button.onclick = function (event){
+            //     dt_Handlers.calculator_handler.add_question(html);
+            // };
         }, 
+
+        // add_question : (html) => {
+        //     let container = document.getElementById("item_container");
+        //     let save = document.getElementById("save");
+        //     let cancel = document.getElementById("cancel");
+        //     console.log(save);
+        // },
 
         generate_details : (obj) => {
             let html = "";
-            html += `<h3> Name  : ${obj.name} </h3>`;
-            html += `<h5> Intro <input type="text" value="${obj.description}"></intro> </h5>`;
+            html += `<h3> Name  : <textarea rows="4" cols="50" id="name_area"> ${obj.name} </textarea></h3>`;
+            html += `<h5> Intro : </h5> <textarea rows="4" cols="50"> ${obj.description} </textarea>  <br>`;
             for(let i = 0; i < obj.questions.length; i++){
-                html += `<p> ${obj.questions[i].text} </p>`;
+                html += `<textarea rows="4" cols="50"> ${obj.questions[i].text} </textarea> `;
                 for(let j = 0; j < obj.questions[i].answers.length; j++){
-                    html += `<p> ${obj.questions[i].answers[j].text} </p>`;
-                    html += `<p> Points : ${obj.questions[i].answers[j].points} </p>`;
+                    html += `<textarea rows="4" cols="50"> ${obj.questions[i].answers[j].text} </textarea>`;
+                    html += `<p> Points : <input type="text" class="points" value="${obj.questions[i].answers[j].points}"> </p>`;
                 }
-                html += `<br\>`
+                html += `<br>`
             }
             html += `<h5> Final : ${obj.answer} </h5>`;
+            // html += '<button id="add_question">Add question</button>';
+            // html += '<button id="cansel">Cancel</button>';
+            // html += '<button id="save">Save</button>';
             return html;
         }
     }
