64098e5cfa6043951ae8e938f8ea042c3a05cbcc
vbalyan02 <vitaliy.balyan@tumo.org>
2021-06-17 18:24:26 +0400

    almost done

--- scripts/AllControllerSystem/ac_details.js
+++ scripts/AllControllerSystem/ac_details.js
@@ -19,6 +19,9 @@
     }
 }
 
+
+
+
 const dt_Handlers = {
     calculator_handler  : {
         temp_obj    : {},
@@ -52,7 +55,9 @@ const dt_Handlers = {
 
         answer_event : (type, i) => {
             let answers_blocks = document.getElementsByClassName("answers_block");
-            answers_blocks[i].innerHTML += dt_Handlers.calculator_handler.add_answer_and_point('','', type, answers_blocks[i].children.length++);   
+            let temp = dt_Handlers.calculator_handler.add_answer_and_point('','', type, answers_blocks[i].children.length++, i);   
+
+            answers_blocks[i].innerHTML += temp;
         },
         
         add_type_question : (type) => {
@@ -60,21 +65,31 @@ const dt_Handlers = {
             let question_types = document.getElementById("question_types");
             let questions_len = document.getElementsByClassName("question_block").length;
             let flex_dir = "column";
-            let class_name;
+            let answer_button;
+            
             if (type == 0)
-                class_name = 'add_answer';
+                answer_button = dt_Handlers.calculator_handler.create_answer_button(type, "add_answer");
+
             else
             {
+                answer_button = dt_Handlers.calculator_handler.create_answer_button(type, "add_answer pic_button");
                 flex_dir = "row";
-                class_name = 'add_pic_answer';
             }
                 
-            let answer_button = `</div ><div class="${class_name}"><div class="add_answer_button" onclick="dt_Handlers.calculator_handler.answer_event(${type}, ${dt_Handlers.calculator_handler.answer_button_index})"> <img class="add_answer_image" src="../../images/+.svg"> </div></div>`;
 
             question_types.style.display = "none";
             let questions_block = document.getElementById("content");
             questions_block.innerHTML += dt_Handlers.calculator_handler.add_question(questions_len + 1, '',  flex_dir) + answer_button;
 
+        },
+
+        create_answer_button : (type, class_name) => {
+            return `<div id="plus_${dt_Handlers.calculator_handler.answer_button_index}" class="${class_name}">
+                        <div class="add_answer_button" onclick="dt_Handlers.calculator_handler.answer_event(${type}, ${dt_Handlers.calculator_handler.answer_button_index})"> 
+                            <img class="add_answer_image" src="../../images/+.svg"> 
+                        </div>
+                    </div> </div> </div> 
+                    <div class="right_icon"> <img src="../../images/x.svg"></div>`;
         },
 
         create_header : (name) => {
@@ -105,7 +120,7 @@ const dt_Handlers = {
 
         add_question : (question_number, question_text, flex_dir) => {
             let question = `    
-            <div class="question_block" id="${dt_Handlers.calculator_handler.question_id++}"> 
+            <div class="question_block" id="question_${dt_Handlers.calculator_handler.question_id++}"> 
                 <div class="question_block_icon"> 
                     <img src="../../images/=.svg"> </div> 
                 <div class="question" > 
@@ -117,48 +132,90 @@ const dt_Handlers = {
                     </div> 
                 </div>
                 <textarea rows="1" cols="50" class="question_name"> ${question_text}</textarea> <br>
-                <div class="answers_block" style="flex-direction:${flex_dir}">`; 
+                <div class="answers_block" style="flex-direction:${flex_dir}">
+                `; 
             return question;
         },
 
-        add_answer_and_point : (text, points, type, id) => {
+        add_answer_and_point : (text, points, type, id, i) => {
             let answer;
             let final_block;
             let point = `<p> Points : </p><input type="text" class="points" value="${points}"> `;
             if (type == 0){
                 let drag_item = `<div class="answer" id="${id}"> <div> <img src="../../images/=.svg"> </div>`;
                 answer = `<textarea rows="1" cols="50" class="answer_text"> ${text} </textarea>`;
-                final_block =  drag_item + answer + point + `<div class="right_icon"> <img src="../../images/x.svg"></div> </div>`;
+                final_block =  drag_item + answer + point + `<div class="right_icon"> <img src="../../images/x.svg"></div> </div> `;
             }
-            else if (type == 2){
+            else {
                 answer = `
                 <div class="pic_answer" id="${id}"> 
                     <div class="image_block">  
-                        <div class="load_picture" onclick="dt_Handlers.calculator_handler.redraw_block(${id}, ${dt_Handlers.calculator_handler.answer_button_index})"> 
+                        <div class="load_picture" onclick="dt_Handlers.calculator_handler.redraw_block(${id}, ${dt_Handlers.calculator_handler.answer_button_index}, ${type})"> 
                             <img class="load_image" src="../../images/arrow.svg"> 
                         </div> 
                     </div>
                 </div>
                 `;
+                let add_button = document.getElementById("plus_"+i);
+                let copy = `<div id="plus_${dt_Handlers.calculator_handler.answer_button_index}" class="add_answer pic_button"><div class="add_answer_button" onclick="dt_Handlers.calculator_handler.answer_event(${type}, ${dt_Handlers.calculator_handler.answer_button_index})"> <img class="add_answer_image" src="../../images/+.svg"> </div></div></div>`;
+                add_button.remove();
+                
+                answer += copy;
                 final_block = answer;
             }
+            
             return final_block;
         },
 
-        redraw_block : (a_id, q_id) => {
+        redraw_block : (a_id, q_id, type) => {
             let answers_blocks = document.getElementsByClassName("answers_block");
-            answers_blocks[q_id].children[a_id].innerHTML = `
-                                                                <div class="delete"> 
-                                                                    <img src="../../images/x.svg">
-                                                                </div>
-                                                                <div class="image_block">  
-                                                                    <div class="image_load_block"></div>
-                                                                    <div class="reload_picture" onclick="dt_Handlers.calculator_handler.reload_image(${a_id}, ${dt_Handlers.calculator_handler.answer_button_index})"> 
-                                                                        <img class="reload_image" src="../../images/arrow.svg"> 
-                                                                    </div> 
-                                                                </div>
-                                                                <div class="pic_points"> <p> Points : </p><input type="text" class="points" value=""> </div>
-                                                            `; 
+            answers_blocks[q_id].children[a_id - 1].innerHTML = `
+                <div class="delete"> 
+                    <img src="../../images/x.svg">
+                </div>
+                <div class="image_block">  
+                    <div class="image_load_block">
+                        <img style="display:none" src="../../images/enot.jpg" class="answer_image" id="pic_answer_${dt_Handlers.calculator_handler.answer_button_index}_${a_id}"></img>
+                    </div>
+                    <div class="reload_picture" onchange="dt_Handlers.calculator_handler.reload_image(${a_id})"> 
+                        <img class="reload_image" src="../../images/arrow.svg" > 
+                        <input type="file" class="reload_image input_image" id="pic_input_${dt_Handlers.calculator_handler.answer_button_index}_${a_id}">
+                    </div> 
+                </div>
+                `; 
+            
+            if (type == 1)
+                answers_blocks[q_id].children[a_id - 1].innerHTML += `<div class="pic_ans_inp"><input type="text" class="points pic_text" ></div>`;
+
+            answers_blocks[q_id].children[a_id - 1].innerHTML += `<div class="pic_points"> <p> Points : </p><input type="text" class="points" value=""> </div>`;
+
+        },
+
+        reload_image : async (a_id) => {
+            // console.log("MTA");
+            // let get_url =  document.location.href;
+            // var url = new URL(get_url);
+            var id = `pic_input_${dt_Handlers.calculator_handler.answer_button_index}_${a_id}`;
+            let info = await dt_Handlers.calculator_handler.input_to_base(id);
+            let ans_image = document.getElementById(`pic_answer_${dt_Handlers.calculator_handler.answer_button_index}_${a_id}`);
+            ans_image.style.display = 'flex';
+            ans_image.src = info;
+        },
+
+        input_to_base : (id) => {
+            if(!id) 
+                return {error : "Id not found"};
+            return new Promise((resolve) => {
+                if (id) {
+                    input = document.getElementById(id);
+                    file = input.files[0];
+                    fr = new FileReader();
+                    fr.readAsDataURL(file);
+                    fr.onloadend = () => {
+                        resolve(fr.result);
+                    }
+                }
+            })
         },
 
         generate_details : (obj) => {
@@ -176,7 +233,7 @@ const dt_Handlers = {
                 an_id = 0;
                 html += `</div>`;
                 html += `<div class="add_answer"><div class="add_answer_button" onclick="dt_Handlers.calculator_handler.answer_event(0, ${qn_id++})">  <img class="add_answer_image" src="../../images/+.svg"> </div></div>`;
-                html += `</div></div>`;
+                html += `</div> <div class="right_icon"> <img src="../../images/x.svg"></div> </div>`;
             }
             
             html += `</div> <div id="add_question"><div id="add_question_button"> <img id="add_question_image" src="../../images/+.svg"> </div>`;
@@ -190,3 +247,41 @@ const dt_Handlers = {
         }
     }
 }
+
+// function enableDragSort(listClass) {
+//     const sortableLists = document.getElementsByClassName(listClass);
+//     console.log(sortableLists);
+//     Array.prototype.map.call(sortableLists, (list) => {enableDragList(list)});
+// }
+  
+// function enableDragList(list) {
+//     Array.prototype.map.call(list.children, (item) => {enableDragItem(item)});
+// }
+  
+// function enableDragItem(item) {
+//     console.log(item);
+//     item.setAttribute('draggable', true)
+//     item.ondrag = handleDrag;
+//     item.ondragend = handleDrop;
+//   }
+  
+//   function handleDrag(item) {
+//     const selectedItem = item.target,
+//           list = selectedItem.parentNode,
+//           x = event.clientX,
+//           y = event.clientY;
+    
+//     selectedItem.classList.add('drag-sort-active');
+//     let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
+    
+//     if (list === swapItem.parentNode) {
+//         swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
+//         list.insertBefore(selectedItem, swapItem);
+//     }
+//   }
+  
+// function handleDrop(item) {
+//     item.target.classList.remove('drag-sort-active');
+// }
+
+// (()=> {enableDragSort('answers_block')})();
